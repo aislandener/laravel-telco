1db5621769f264fbe7cee9a1b6be221158b75cc0
Aislan Dener Souza Vicentini <aislandenersouzavicentini@gmail.com>
2024-01-18 11:10:16 -0300

    fix: in omnichannel service, if availableSchedulingDates error in code 200, throw TelcoException

--- src/Services/OmnichannelService.php
+++ src/Services/OmnichannelService.php
@@ -2,6 +2,7 @@
 
 namespace Aislandener\Telco\Services;
 
+use Aislandener\Telco\Exceptions\TelcoException;
 use Carbon\Carbon;
 use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Support\Collection;
@@ -12,17 +13,37 @@ class OmnichannelService
     public function __construct(private readonly PendingRequest $http)
     {}
 
+    /**
+     * @param int $serviceId
+     * @param Carbon $date
+     * @param ShiftInstallation $shift
+     * @param int $days
+     * @return Collection
+     * @throws TelcoException
+     */
     public function availableSchedulingDates(int $serviceId, Carbon $date, ShiftInstallation $shift, $days = 7): Collection
     {
-        return collect($this->http->withUrlParameters([
+        $data = $this->http->withUrlParameters([
             'serviceId' => $serviceId,
             'date' => $date->format('dmY'),
             'shift' => $shift->apiInfo()
         ])->get('ws/integracao/omnichannel/agenda_tecnica/configuracoes/atendimento/{serviceId}/data/{date}/turno/{shift}', [
             'qtdeDiasLimite' => $days
-        ])->json('data'));
+        ]);
+
+        if($data->json('error')){
+            throw new TelcoException($data->json('error'), 422);
+        }
+
+        return $data->json('data');
     }
 
+    /**
+     * @param int $serviceId
+     * @param int $dateId
+     * @return Collection
+     * @throws TelcoException
+     */
     public function scheduleInstallation(int $serviceId, int $dateId): Collection
     {
         return collect($this->http->put('ws/integracao/omnichannel/agenda_tecnica/configuracoes/efetivar', [
