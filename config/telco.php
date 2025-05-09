adff22cd0991effb8417c0b6a8f8c9bae92fb645
Aislan Dener Souza Vicentini <aislandenersouzavicentini@gmail.com>
2025-05-09 11:24:56 -0300

    release: v2.1.16

--- config/telco.php
+++ config/telco.php
@@ -4,6 +4,7 @@ return [
     'url' => env('TELCO_URL', 'https://teste.adapter.net.br/'),
     'username' => env('TELCO_USERNAME'),
     'password' => env('TELCO_PASSWORD'),
+    'timeout' => env('TELCO_TIMEOUT', 30),
     'recurrence' => [
         'key' => env('TELCO_CRYPT_KEY'),
         'cipher' => env('TELCO_CRYPT_CIPHER'),
